24cb911acd88ab62ae7d4f4668d530d755e18f7c
Louis G. Pang <lpang@princeton.edu>
2022-03-20 05:20:22 -0400

    untested sql

---
+++ RawData_SQL.py
@@ -0,0 +1,96 @@
+from sys import stderr, exit
+import psycopg2
+
+def accountCreation(net_id, year, maj):
+    try:
+        connect = psycopg2.connect(host = "localhost",
+                                   database = "TigerFriend",
+                                   user = "postgres",
+                                   password = "RNCHL")
+
+        cursor = connect.cursor()
+        stmt = "INSERT INTO RawData (net_id, class_year, major) VALUE"
+        stmt += "((%s), (%s), (%s))"
+        cursor.execute(stmt, (net_id, year, maj))
+
+        connect.commit()
+        count = cursor.rowcount
+        print(count, "Record inserted successfully into RawData")
+
+    except (Exception, psycopg2.Error) as ex:
+        print(ex, file=stderr)
+        exit(1)
+    finally:
+        if(connect):
+            cursor.close()
+            connect.close()
+
+def accountPopulation(net_id, user, bio, q1, q2):
+    try:
+        connect = psycopg2.connect(host = "localhost",
+                                   database = "TigerFriend",
+                                   user = "postgres",
+                                   password = "RNCHL")
+
+        cursor = connect.cursor()
+        stmt = "UPDATE RawData SET username = (%s), "
+        stmt += "bio_string = (%s), "
+        stmt += "survery_q1_response = (%s), "
+        stmt += "survery_q1_response = (%s), "
+        stmt += "WHERE net_id = (%s)"
+        cursor.execute(stmt, (user, bio, q1, q2, net_id))
+
+        connect.commit()
+        count = cursor.rowcount
+        print(count, "Record inserted successfully into RawData")
+
+    except (Exception, psycopg2.Error) as ex:
+        print(ex, file=stderr)
+        exit(1)
+    finally:
+        if(connect):
+            cursor.close()
+            connect.close()
+
+def updateBio(net_id, bio):
+    try:
+        connect = psycopg2.connect(host = "localhost",
+                                   database = "TigerFriend",
+                                   user = "postgres",
+                                   password = "RNCHL")
+
+        cursor = connect.cursor()
+        stmt = "UPDATE RawData SET bio_string = (%s)"
+        stmt += "WHERE net_id = (%s)"
+        cursor.execute(stmt, (bio, net_id))
+
+        connect.commit()
+        count = cursor.rowcount
+        print(count, "Record inserted successfully into RawData")
+    
+    except (Exception, psycopg2.Error) as ex:
+        print(ex, file=stderr)
+        exit(1)
+    finally:
+        if(connect):
+            cursor.close()
+            connect.close()
+
+def updateUsername(net_id, user):
+    try:
+        connect = psycopg2.connect(host = "localhost",
+                                   database = "TigerFriend",
+                                   user = "postgres",
+                                   password = "RNCHL")
+        cursor = connect.cursor()
+        stmt = "UPDATE RawData SET username = (%s)"
+        stmt += "WHERE net_id = (%s)"
+        cursor.execute(stmt, (user, net_id))
+    
+    except (Exception, psycopg2.Error) as ex:
+        print(ex, file=stderr)
+        exit(1)
+    finally:
+        if(connect):
+            cursor.close()
+            connect.close()
