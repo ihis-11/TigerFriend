b29548d45e84e0655803c0ac672f52411ce02c15
nbelova-cos <nbelova@princeton.edu>
2022-03-24 22:38:29 -0400

    Deployed database use

--- RawData_SQL.py
+++ RawData_SQL.py
@@ -3,23 +3,23 @@ import psycopg2
 
 def account_creation(net_id, year, maj):
     try:
-        with psycopg2.connect(host = "10.8.53.63",
-                                   database = "TigerFriend",
-                                   user = "postgres",
-                                   password = "RNCHL") as connect:
+        with psycopg2.connect(host = "ec2-3-229-161-70.compute-1.amazonaws.com",
+                                   database = "d2fdvi8f5tvpvo",
+                                   user = "yfdafrxedkbxza",
+                                   password = "3768ffff6c40b7ca1d4274e6d428b9adbd6c5d8becd30b6c479236de989a8f1e") as connect:
             if connect is not None:
                 print("CONNECTED TO POSTGRESQL")
             else: 
                 print(connect)
                 raise Exception("Connect was null")
             with connect.cursor() as cursor:
-                stmt = "INSERT INTO RawData (net_id, class_year, major) VALUES (\'" + net_id + "\', \'" + year + "\', \'" + maj + "\');"
+                stmt = "INSERT INTO account (net_id, class_year, major) VALUES (\'" + net_id + "\', \'" + year + "\', \'" + maj + "\');"
                 print(stmt)
                 cursor.execute(stmt)
 
                 connect.commit()
                 count = cursor.rowcount
-                print(count, "Record inserted successfully into RawData")
+                print(count, "Record inserted successfully into account")
 
     except (Exception, psycopg2.Error) as ex:
         print(ex, file=stderr)
@@ -32,13 +32,13 @@ def account_creation(net_id, year, maj):
 
 def account_population(net_id, user, bio, q1, q2):
     try:
-        with psycopg2.connect(host = "10.8.53.63",
-                                   database = "TigerFriend",
-                                   user = "postgres",
-                                   password = "RNCHL") as connect:
+        with psycopg2.connect(host = "ec2-3-229-161-70.compute-1.amazonaws.com",
+                                   database = "d2fdvi8f5tvpvo",
+                                   user = "yfdafrxedkbxza",
+                                   password = "3768ffff6c40b7ca1d4274e6d428b9adbd6c5d8becd30b6c479236de989a8f1e") as connect:
 
             with connect.cursor() as cursor:
-                stmt = "UPDATE RawData SET username = (%s), "
+                stmt = "UPDATE Raccount SET username = (%s), "
                 stmt += "bio_string = (%s), "
                 stmt += "survery_q1_response = (%s), "
                 stmt += "survery_q1_response = (%s), "
@@ -47,7 +47,7 @@ def account_population(net_id, user, bio, q1, q2):
 
                 connect.commit()
                 count = cursor.rowcount
-                print(count, "Record inserted successfully into RawData")
+                print(count, "Record inserted successfully into account")
 
     except (Exception, psycopg2.Error) as ex:
         print(ex, file=stderr)
@@ -55,19 +55,19 @@ def account_population(net_id, user, bio, q1, q2):
 
 def update_bio(net_id, bio):
     try:
-        with psycopg2.connect(host = "10.8.53.63",
-                                   database = "TigerFriend",
-                                   user = "postgres",
-                                   password = "RNCHL") as connect:
+        with psycopg2.connect(host = "ec2-3-229-161-70.compute-1.amazonaws.com",
+                                   database = "d2fdvi8f5tvpvo",
+                                   user = "yfdafrxedkbxza",
+                                   password = "3768ffff6c40b7ca1d4274e6d428b9adbd6c5d8becd30b6c479236de989a8f1e") as connect:
 
             with connect.cursor() as cursor:
-                stmt = "UPDATE RawData SET bio_string = (%s)"
+                stmt = "UPDATE account SET bio_string = (%s)"
                 stmt += "WHERE net_id = (%s)"
                 cursor.execute(stmt, (bio, net_id))
 
                 connect.commit()
                 count = cursor.rowcount
-                print(count, "Record inserted successfully into RawData")
+                print(count, "Record inserted successfully into account")
     
     except (Exception, psycopg2.Error) as ex:
         print(ex, file=stderr)
@@ -75,12 +75,12 @@ def update_bio(net_id, bio):
 
 def update_username(net_id, user):
     try:
-        with psycopg2.connect(host = "10.8.53.63",
-                                   database = "TigerFriend",
-                                   user = "postgres",
-                                   password = "RNCHL") as connect:
+        with psycopg2.connect(host = "ec2-3-229-161-70.compute-1.amazonaws.com",
+                                   database = "d2fdvi8f5tvpvo",
+                                   user = "yfdafrxedkbxza",
+                                   password = "3768ffff6c40b7ca1d4274e6d428b9adbd6c5d8becd30b6c479236de989a8f1e") as connect:
             with connect.cursor() as cursor:
-                stmt = "UPDATE RawData SET username = (%s)"
+                stmt = "UPDATE account SET username = (%s)"
                 stmt += "WHERE net_id = (%s)"
                 cursor.execute(stmt, (user, net_id))
     
@@ -90,12 +90,12 @@ def update_username(net_id, user):
 
 def get_user_data(net_id): # returns year and major
     try:
-        with psycopg2.connect(host = "10.8.53.63",
-                                   database = "TigerFriend",
-                                   user = "postgres",
-                                   password = "RNCHL") as connect:
+        with psycopg2.connect(host = "ec2-3-229-161-70.compute-1.amazonaws.com",
+                                   database = "d2fdvi8f5tvpvo",
+                                   user = "yfdafrxedkbxza",
+                                   password = "3768ffff6c40b7ca1d4274e6d428b9adbd6c5d8becd30b6c479236de989a8f1e") as connect:
             with connect.cursor() as cursor:
-                stmt = "SELECT class_year, major FROM RawData WHERE net_id = \'" + net_id + "\'"
+                stmt = "SELECT class_year, major FROM account WHERE net_id = \'" + net_id + "\'"
                 print(stmt)
                 cursor.execute(stmt)
 
