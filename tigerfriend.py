ee241ad1cd637a578b4db8e59ad66178dbb1e91a
nbelova-cos <nbelova@princeton.edu>
2022-03-31 21:01:35 -0400

    import auth move

--- tigerfriend.py
+++ tigerfriend.py
@@ -8,7 +8,6 @@ from matching import input_match_scores, get_matches
 from keys import APP_SECRET_KEY
 from req_lib import getOneUndergrad
 import psycopg2
-import auth
 from sys import stderr
 
 # --------------------------------------------------------------------
@@ -16,6 +15,8 @@ from sys import stderr
 app = Flask(__name__, template_folder='templates')
 
 app.secret_key = APP_SECRET_KEY
+
+import auth
 
 # --------------------------------------------------------------------
 
