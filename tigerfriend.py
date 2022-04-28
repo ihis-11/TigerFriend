3e5002b0c0d61aa16f7193543a092ebc2d847263
Crystal Pan <cp30@princeton.edu>
2022-04-28 02:41:14 -0400

    question stats

--- tigerfriend.py
+++ tigerfriend.py
@@ -26,11 +26,11 @@ import auth
 
 # -----------------------------------------------------------------------
 
-@app.before_request
-def before_request():
-    if not request.is_secure:
-        url = request.url.replace('http://', 'https://', 1)
-        return redirect(url, code=301)
+# @app.before_request
+# def before_request():
+#     if not request.is_secure:
+#         url = request.url.replace('http://', 'https://', 1)
+#         return redirect(url, code=301)
 
 # --------------------------------------------------------------------
 
